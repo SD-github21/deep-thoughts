f621fda787bf36fca2a353e3dfebee60240c8318
SD-github21 <sapandonde.gmail.com>
2022-08-04 14:36:59 -0500

    created form component

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -7,6 +7,7 @@ import Auth from '../utils/auth';
 
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
+import ThoughtForm from '../components/ThoughtForm';
 
 
 const Profile = () => {
@@ -73,6 +74,7 @@ const Profile = () => {
           />
         </div>
       </div>
+      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
